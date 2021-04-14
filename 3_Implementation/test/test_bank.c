a2647b425cb40ae17da76ff2fa06ce24ddee041c
Shriya-265054 <shriya.naik0309@gmail.com>
2021-04-15 02:17:46 +0530

    update

--- 3_Implementation/test/test_bank.c
+++ 3_Implementation/test/test_bank.c
@@ -13,9 +13,14 @@ void tearDown()
 }
 void test_add(void)
 {
-    acc=add(acc,'ABC',123456,21,'Pune','Indian',85697562,2000.00,'Saving',2000,4,9,2021);
+    acc=add(acc,'ABC',123456,21,'xyz','IN',85697562,'S',2000.00,492021);
     TEST_ASSERT_EQUAL(123456,acc->acc_no);
     TEST_ASSERT_EQUAL(21,acc->age);
+}
+
+void test_deposit(void)
+{
+    
 }
 
 int main(void)
@@ -23,6 +28,7 @@ int main(void)
     UNITY_BEGIN();
 
     RUN_TEST(test_add);
+    RUN_TEST(test_deposit);
 
     return UNITY_END();
 }
