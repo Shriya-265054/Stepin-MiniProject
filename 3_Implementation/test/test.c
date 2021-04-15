5152dcbfc508ffbf07c7f11990f48cc6226c4387
Shriya Prashant Naik <32011879+Shriya-265054@users.noreply.github.com>
2021-04-15 15:54:11 +0530

    Create test.c

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -11,6 +11,17 @@ void tearDown()
 {
 
 }
+
+char name[60];
+int age;
+char address[100];
+char citizenship[20];
+int phone;
+char acc_type[10];
+float amt;
+int month;
+int day;
+int year;
 void test_newacc(void)
 {
     acc=newacc(acc,"ABC",123456,21,"xyz","IN",85697562,"S",2000.00,4,9,2021);
@@ -30,8 +41,18 @@ void test_search(void)
 }
 void test_add(void)
 {
-   acc=add(acc,"pqr",1234,45,"def","NYC",256864634,"C",50000.00,3,9,1999);
+    acc_no = 1234 ;
+    strcpy(name, "def");
+    strcpy(address, "pqr");
+    strcpy(citizenship,"NYC");
+    strcpy(acc_type, "C");
+    age=45;
+    phone=256864634;
+    amt=50000.00;
+    day=3;month=9;year=1999;
+   TEST_ASSERT_EQUAL(SUCCESS,add(acc,name,acc_no,age,address,citizenship,phone,acc_type,amt,day,month,year));
    TEST_ASSERT_EQUAL("pqr",acc->name);
+   TEST_ASSERT_EQUAL(45,acc->age);
 }
 
 int main(void)
