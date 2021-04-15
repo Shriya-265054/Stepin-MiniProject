201726a50ce3f880aa3d3944227474501ad23942
Shriya-265054 <shriya.naik0309@gmail.com>
2021-04-15 13:39:00 +0530

    test

--- 3_Implementation/test/test_bank.c
+++ 3_Implementation/test/test_bank.c
@@ -11,24 +11,36 @@ void tearDown()
 {
 
 }
-void test_add(void)
+void test_newacc(void)
 {
-    acc=add(acc,'ABC',123456,21,'xyz','IN',85697562,'S',2000.00,492021);
+    acc=newacc(acc,'ABC',123456,21,'xyz','IN',85697562,'S',2000.00,4,9,2021);
     TEST_ASSERT_EQUAL(123456,acc->acc_no);
     TEST_ASSERT_EQUAL(21,acc->age);
 }
-
-void test_deposit(void)
+void test_search(void)
 {
-    
+    info temp={0};
+    int cust_id=123;
+    int see=0;
+    TEST_ASSERT_EQUAL(SUCCESS,search(acc,cust_id,&temp,&see));
+    TEST_ASSERT_EQUAL(1,see);
+    cust_id=12;
+    search(acc,cust_id,&temp,&see);
+    TEST_ASSERT_EQUAL(0,see);
+}
+void test_add(void)
+{
+   TEST_ASSERT_EQUAL(SUCCESS,add(acc,"pqr",1234,45,"def","NYC",256864634,"C",50000.00,3,9,1999));
+   TEST_ASSERT_EQUAL("pqr",acc->name);
 }
 
 int main(void)
 {
     UNITY_BEGIN();
 
+    RUN_TEST(test_newacc);
     RUN_TEST(test_add);
-    RUN_TEST(test_deposit);
+    RUN_TEST(test_search);
 
     return UNITY_END();
 }
